6ce5c9608a70fc1775a72ec56dc876e03c3c314c
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-21 12:51:14 +0300

    swap urls

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import custom_404, custom_500
@@ -6,6 +7,27 @@ handler404 = custom_404
 handler500 = custom_500
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('auth/registration/', views.RegistrationView.as_view(), name='registration'),
+
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
     path('', include('blog.urls')),
