b379e8da6287ecd2a66d6e41efe1db96fd387787
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-21 13:09:44 +0300

    add registration page

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import path, include, reverse_lazy
+from django.views.generic import CreateView
+
 from pages.views import custom_404, custom_500
 
 handler404 = custom_404
@@ -26,9 +29,18 @@ urlpatterns = [
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
-    path('auth/registration/', views.RegistrationView.as_view(), name='registration'),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
 
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
     path('', include('blog.urls')),
+
 ]
