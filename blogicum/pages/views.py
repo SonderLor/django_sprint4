99abbf1d7db968262eea45aa4b45848382bc886e
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-21 12:47:18 +0300

    add custom handlers

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+def custom_403_csrf(request, reason=""):
+    return render(request, "pages/403_csrf.html", status=403)
+
+
+def custom_404(request, exception):
+    return render(request, "pages/404.html", status=404)
+
+
+def custom_500(request):
+    return render(request, "pages/500.html", status=500)
+
+
 def about(request):
     template = 'pages/about.html'
     return render(request, template)
