515150c70de19368446964624dac637f99e59115
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-22 11:16:39 +0300

    switch to CBV and add pagination

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from blog.forms import UserEditForm
 from blog.models import Post, Category
@@ -12,35 +12,42 @@ from blog.utils import get_relevant_posts
 User = get_user_model()
 
 
-def index(request):
-    template = 'blog/index.html'
+class IndexView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "post_list"
+    paginate_by = 10
 
-    post_list = get_relevant_posts(Post.objects)[0:5]
-
-    context = {'post_list': post_list}
-    return render(request, template, context)
-
-
-def post_detail(request, post_id):
-    template = 'blog/detail.html'
-
-    post = get_object_or_404(get_relevant_posts(Post.objects), id=post_id)
-
-    context = {'post': post}
-    return render(request, template, context)
+    def get_queryset(self):
+        return get_relevant_posts(Post.objects)
 
 
-def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    category = get_object_or_404(
-        Category.objects.filter(is_published=True), slug=category_slug
-    )
-    post_list = get_relevant_posts(
-        category.posts.filter(category=category)
-    )
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
 
-    context = {'category': category, 'post_list': post_list}
-    return render(request, template, context)
+    def get_object(self):
+        post_id = self.kwargs.get("post_id")
+        return get_object_or_404(get_relevant_posts(Post.objects), id=post_id)
+
+
+class CategoryPostsView(ListView):
+    model = Post
+    template_name = "blog/category.html"
+    context_object_name = "post_list"
+    paginate_by = 5
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get("category_slug")
+        category = get_object_or_404(Category.objects.filter(is_published=True), slug=category_slug)
+        return get_relevant_posts(category.posts.filter(category=category))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = get_object_or_404(Category.objects.filter(is_published=True),
+                                                slug=self.kwargs.get("category_slug"))
+        return context
 
 
 class UserProfileView(DetailView):
