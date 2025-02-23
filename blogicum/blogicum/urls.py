96bf6c4da90c9785fd35ac46d6b31560d10f44b5
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-23 15:50:08 +0300

    urls indents fix 2

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -12,45 +12,70 @@ handler500 = 'pages.views.custom_500'
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 
-    path('auth/login/',
+    path(
+        'auth/login/',
         auth_views.LoginView.as_view(
-        template_name='registration/login.html'
-    ), name='login'),
-    path('auth/logout/',
+            template_name='registration/login.html'
+        ),
+        name='login'
+    ),
+    path(
+        'auth/logout/',
         auth_views.LogoutView.as_view(
-        template_name='registration/logged_out.html'
-    ), name='logout'),
-    path('auth/password_change/',
+            template_name='registration/logged_out.html'
+        ),
+        name='logout'
+    ),
+    path(
+        'auth/password_change/',
         auth_views.PasswordChangeView.as_view(
-           template_name='registration/password_change_form.html'
-        ), name='password_change'),
-    path('auth/password_change/done/',
+            template_name='registration/password_change_form.html'
+        ),
+        name='password_change'
+    ),
+    path(
+        'auth/password_change/done/',
         auth_views.PasswordChangeDoneView.as_view(
-           template_name='registration/password_change_done.html'),
-        name='password_change_done'),
-    path('auth/password_reset/',
+            template_name='registration/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
+    path(
+        'auth/password_reset/',
         auth_views.PasswordResetView.as_view(
-           template_name='registration/password_reset_form.html'
-        ), name='password_reset'),
-    path('auth/password_reset/done/',
+            template_name='registration/password_reset_form.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'auth/password_reset/done/',
         auth_views.PasswordResetDoneView.as_view(
             template_name='registration/password_reset_done.html'
-        ), name='password_reset_done'),
-    path('auth/reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-           template_name='registration/password_reset_confirm.html'),
-        name='password_reset_confirm'),
-    path('auth/reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(
-           template_name='registration/password_reset_complete.html'),
-        name='password_reset_complete'),
-    path('auth/registration/',
-        CreateView.as_view(
-          template_name='registration/registration_form.html',
-          form_class=UserCreationForm,
-          success_url=reverse_lazy('blog:index'),
         ),
-        name='registration',
+        name='password_reset_done'
+    ),
+    path(
+        'auth/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'auth/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration'
     ),
 
     path('admin/', admin.site.urls),
